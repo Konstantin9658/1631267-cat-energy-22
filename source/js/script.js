7f2373568a7fbbfa0496d5792054035ec14e57cc
Sokolovskiy Konstantin <Super.Juke88@yandex.ru>
2021-05-06 00:26:22 +0400

    Декоративные Элементы страницы формы

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,6 @@
 const btnHeader = document.querySelector('.page-header__button');
-const mainNav = document.querySelector('.main-nav__menu');
+const mainNavMenu = document.querySelector('.main-nav__menu');
+const mainNav =  document.querySelector('.main-nav');
 
 mainNav.classList.remove('main-nav--nojs');
 
@@ -7,11 +8,11 @@ btnHeader.addEventListener('click', function () {
   if (btnHeader.classList.contains('page-header__button--burger')) {
     btnHeader.classList.remove('page-header__button--burger');
     btnHeader.classList.add('page-header__button--closed');
-    mainNav.classList.add('main-nav__menu--show');
+    mainNavMenu.classList.add('main-nav__menu--show');
   } else {
     btnHeader.classList.remove('page-header__button--closed');
     btnHeader.classList.add('page-header__button--burger');
-    mainNav.classList.remove('main-nav__menu--show');
+    mainNavMenu.classList.remove('main-nav__menu--show');
   }
 }
 );
